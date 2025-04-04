f36cd76bd7b49f2605798605d7967027d6f5bcde
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2025-04-04 08:34:15 +0600

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,126 +1,30 @@
 import random
-HANGMANPICS = ['''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''',
- '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''',
- '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''',
- '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''',
- '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''',
- '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''',
- '''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
-print(''' 
- ___________.._______
-| .__________))______|
-| | / /      ||
-| |/ /       ||
-| | /        ||.-''.
-| |/         |/  _  ;
-| |          ||  `/,|
-| |          (\`_.'
-| |         .-`--'.
-| |        /Y . . Y.
-| |       // |   | ;;
-| |      //  | . |  ;;
-| |     ')   |   |   (`
-| |          ||'||
-| |          || ||
-| |          || ||
-| |          || ||
-| |         / | | ;
-""""""""""|_`-' `-' |"""|
-|"|"""""""\ \       '"|"|
-| |        \ \        | |
-: :         \ \       : :
-. .          `'       . .
-      
+import ascii_art
 
-888                                                           
-888                                                           
-888                                                           
-88888b.  8888b. 88888b.  .d88b. 88888b.d88b.  8888b. 88888b.  
-888 "88b    "88b888 "88bd88P"88b888 "888 "88b    "88b888 "88b 
-888  888.d888888888  888888  888888  888  888.d888888888  888 
-888  888888  888888  888Y88b 888888  888  888888  888888  888 
-888  888"Y888888888  888 "Y88888888  888  888"Y888888888  888 
-                             888                              
-                        Y8b d88P                              
-                         "Y88P"    
+ascii_art.title()
 
-
-      ''')
-words=["apple","banana","egg","fish"]
-word=random.choice(words)
-print(word)
-index=[]
-dead=0
-ok=False
-for _ in range(len(word)):
-    index.append('_')
-while dead<7:
-    guess_a_letter=(input("Guess a letter: "))
-    print("".join(index))
-    found= False
-    for i in range(0,len(word)):
-      if guess_a_letter==word[i]:
-        index[i]=guess_a_letter
-        found=True
-    if found==False:
-        print(f"'{guess_a_letter}' is not found this is word")
-        dead+=1
-        ok=True
-    if ok:
-     if dead>=0 and dead<=6:
-        print(HANGMANPICS[dead])
-    print("".join(index))
-    if "".join(index)==word:
-        print("You won")
-        break
-
-if dead==7: print("You lose!!")
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+print(f'Pssst, the solution is {chosen_word}.')
+display = []
+word_length = len(chosen_word)
+lives=6
+for _ in range(word_length):
+    display += "_"
+end_of_game=False
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    print(display)
+    if '_' not in display:
+        print("You win.")
+        end_of_game=True
+    if guess not in chosen_word:
+        print(ascii_art.stages[lives])
+        lives-=1
+        if lives==0:
+            end_of_game=True
+            print("You lose.")
