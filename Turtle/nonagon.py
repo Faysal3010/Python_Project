56831e677793e7900e1bb2e7f733d7146126b3ce
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2025-05-02 16:57:53 +0600

    Create nonagon.py

---
+++ Turtle/nonagon.py
@@ -0,0 +1,19 @@
+from turtle import Turtle,Screen
+sc=Screen()
+t=Turtle()
+l=100
+t.width(5)
+ang=40
+while True:
+    t.fd(l)
+    # t.pu()
+    # t.fd(l)
+    # t.pd()
+    t.left(ang)
+    if abs(t.pos())<1:
+        break
+
+
+
+print(t.pos())
+sc.exitonclick()
