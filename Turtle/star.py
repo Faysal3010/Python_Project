6b0f0712be931d7de2d4c7c008a9c05891e598f2
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2025-05-02 12:08:34 +0600

    Create star.py

---
+++ Turtle/star.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+import random
+
+t = Turtle()
+t.width(5)
+l=5
+colors = ["red", "green", "blue", "orange", "purple", "pink", "yellow"]
+
+for _ in range(100):
+    color=random.choice(colors)
+    t.color(color)
+    t.right(145)
+    t.fd(l)
+    l+=5
+    
+
+screen = Screen()
+screen.exitonclick()
