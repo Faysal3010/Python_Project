6bcdd7cd8c69d0c05db560d4fe26f744d52afc83
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2025-06-05 11:29:39 +0600

    Update main.py

--- Mail Merge Project/main.py
+++ Mail Merge Project/main.py
@@ -3,13 +3,13 @@
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
 
-with open("7.4 mail-merge-project-start/Input/Letters/starting_letter.docx") as file:
+with open("Input/Letters/starting_letter.docx") as file:
     starting_letter=file.read()
-with open("7.4 mail-merge-project-start/Input/Names/invited_names.txt") as file:
+with open("Input/Names/invited_names.txt") as file:
     invited_names=file.readlines()
 length=len(invited_names)
 
 for i in range(length):
     new_contents=starting_letter.replace("[name]",invited_names[i][:-1])
-    with open(f"7.4 mail-merge-project-start/Output/ReadyToSend/{invited_names[i][:-1]}.docx",mode='w') as file:
+    with open(f"Output/ReadyToSend/{invited_names[i][:-1]}.docx",mode='w') as file:
         file.write(new_contents)
