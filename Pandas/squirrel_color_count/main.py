3f9c0e4bfb1e741031f5551fcfe0c5d85ca531d2
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2025-06-06 17:00:42 +0600

    Add files via upload

--- Pandas/squirrel_color_count/main.py
+++ Pandas/squirrel_color_count/main.py
@@ -1,1 +1,15 @@
+import pandas as pd
 
+data=pd.read_csv("Pandas/2018_Central_Park_Squirrel_Census.csv")
+
+grey_squirrel=len(data[data["Primary Fur Color"]=="Gray"])
+red_squirrel=len(data[data["Primary Fur Color"]=="Cinnamon"])
+black_squirrel=len(data[data["Primary Fur Color"]=="Black"])
+
+squirrel_dict={
+    'colors':['Gray','Red','Black'],
+    'count':[grey_squirrel,red_squirrel,black_squirrel]
+}
+
+df=pd.DataFrame(squirrel_dict)
+df.to_csv("squirrel_color_count.csv")
